e29ed9a396517af9415be4d6882b6acfeb0f1e25
Ingegneria Informatica e Automatica <lucy.espo96@gmail.com>
2018-09-12 11:52:31 +0200

    default profile image

--- Travelling-People/app/models/user.rb
+++ Travelling-People/app/models/user.rb
@@ -12,7 +12,16 @@ class User < ApplicationRecord
 	def self.from_omniauth(auth) 
 		where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
 			user.email = auth.info.email
+			user.fotoProfilo = auth.info.image
 		end
+	end
+
+	def self.new_with_session(params, session) 
+		super.tap do |user|
+			if data = session["devise.google_data"] && session["devise.google_data"]["extra"]["raw_info"]
+				user.email = data["email"] if user.email.blank?
+			end
+   	end
 	end
 
 	private
