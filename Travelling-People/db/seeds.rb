a63f5b468ea441920248b90845f0d3c341f45d35
Alessandro Biondi <alessandro@biondi.me>
2018-09-16 06:24:52 +0200

    Added basic report support

--- Travelling-People/db/seeds.rb
+++ Travelling-People/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+User.create!([{
+  username: "Test",
+  descrizione: "Seeded user",
+  email: "test@test.com",
+  password: "pass123",
+  password_confirmation: "pass123"
+  }])
+
+Report.create!([{
+  title: "My Report",
+  content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  user_id: User.first.id
+  }])
