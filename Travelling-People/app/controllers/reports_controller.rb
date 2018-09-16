c452a3dc5bd8d90bdc0b91300e7fa113b521c4ae
Alessandro Biondi <alessandro@biondi.me>
2018-09-16 10:51:22 +0200

    Users are now able to view and create reports. (Reports are showed on user's personal page)

--- Travelling-People/app/controllers/reports_controller.rb
+++ Travelling-People/app/controllers/reports_controller.rb
@@ -1,7 +1,30 @@
 class ReportsController < ApplicationController
 
+  before_action :redirect_if_not_signed_in, only: [:new]
+
   def index
     @reports = Report.all
+  end
+
+  def show
+  end
+
+  def new
+    @report = Report.new
+  end
+
+  def create
+    @report = Report.new(report_params)
+    if @report.save
+      redirect_to user_path(current_user)
+    else
+      redirect_to root_path
+    end
+  end
+
+  def report_params
+    params.require(:report).permit(:content, :title)
+                         .merge(user_id: current_user.id)
   end
 
 end
