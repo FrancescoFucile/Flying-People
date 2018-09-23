8d120cf54d32e1d073f8fda58fee08f06efe1c16
FrancescoFucile <30294241+FrancescoFucile@users.noreply.github.com>
2018-09-23 18:26:23 +0200

    FIX: users#shows modified to request id in place of username.

--- Travelling-People/app/controllers/users_controller.rb
+++ Travelling-People/app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
 	end
 
 	def show
-		@user = User.find_by(username: params[:user][:username])
+	#	@user = User.find_by(username: params[:user][:username])
+		@user = User.find(params[:id])
 		if @user.nil?
 			flash[:warning] = 'Utente inesistente!'
 			redirect_to root_path
