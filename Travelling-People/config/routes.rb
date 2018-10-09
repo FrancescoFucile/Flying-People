031221cf5155a693fb36e85fa2db38672df0cbad
biosan <alessandro@biondi.me>
2018-10-10 01:03:26 +0200

    Better routes organization

--- Travelling-People/config/routes.rb
+++ Travelling-People/config/routes.rb
@@ -11,12 +11,19 @@ Rails.application.routes.draw do
 	resources :message_abuses, only:[:new, :create, :index]
 	
 	get 'users/search', to: 'users#search'
-	get 'reports/:id/like', to: 'reports#like', as: 'report_like'
   get 'photos/:id/like', to: 'photos#like', as: 'photo_like'
   post 'photos/:id/comment', to: 'photos#comment', as: 'photo_comments'
 
-	root :to => 'home#index'
+	root :to => 'home#index', as: 'root'
 
-    resources :reports
+	# Reports stuff
+	resources :reports do
+		resources :report_comments
+	end
+	get  'reports/:id/like', 				 to: 'reports#like',           as: 'report_like'
+	get  'reports/:id/dislike', 	   to: 'reports#dislike',        as: 'report_dislike'
+	#post 'reports/:id/comment', 		 to: 'report_comments#create', as: 'report_comment'
+	#get  'reports/:id/comment/:cid', to: 'report_comments#show',   as: 'report_comment_show'
+
 
 end
