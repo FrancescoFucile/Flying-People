0cc4247b1415ce2cc48ee06ca5905685f9a95671
biosan <alessandro@biondi.me>
2018-10-26 11:09:49 +0200

    Fix locality in seeds

--- Travelling-People/db/seeds.rb
+++ Travelling-People/db/seeds.rb
@@ -31,7 +31,8 @@ User.create!([{
 Report.create!(
   title:   "Lorem ipsum",
   content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  user_id: User.find_by(username: "Bob").id
+  user_id: User.find_by(username: "Bob").id,
+  locality: "Rome"
   )
 
 ReportLike.create!(
